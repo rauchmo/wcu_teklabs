df7f8dcaa38d0d5de897ad09a49a6cf12ba7aade
Krapfal <rauchmo@gmail.com>
2022-09-17 14:44:15 +0200

    Create projekt_db.py

---
+++ projekt_db.py
@@ -0,0 +1,17 @@
+import time
+
+while True:
+  x = input("Enter a projekt_number: ")
+  print("Searching... Please wait")
+  time.sleep(3)
+  if x == "6969420":
+      print("Projekt "Martin Man Control Device" found")
+      y = input("Download Plans (y/n):")
+      if (y == "y"):
+        z = 0
+        while (z != 100): 
+          print("Downloading Plans "+z+"%")
+          z = z + 1
+          print("Download DONE")
+      else:
+        print("Abort")  
